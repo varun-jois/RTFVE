76194fb9c4b7cf7e3e1cd90f61eb895161655ebf
varun-jois <varunrameshjois@gmail.com>
2025-09-18 00:03:20 -0400

    Removed some unused models.

--- models/discriminators.py
+++ models/discriminators.py
@@ -1,12 +1,7 @@
-import torch
 from torch import Tensor
 import torch.nn as nn
-import torch.nn.functional as F
-from typing import Any, Callable, List, Optional
 from torch import Tensor
-from typing import List
 from models.RTFVE import FeatureBlock
-import models.basicblock as B
 
 
 class Shuffle_Discriminator(nn.Module):
@@ -29,21 +24,3 @@ class Shuffle_Discriminator_Norm(nn.Module):
 
     def forward(self, x: Tensor):
         return self.model(self.unshuffle(x))
-
-
-class ResNet_Discriminator(nn.Module):
-
-    def __init__(self, nc, nb):
-        super().__init__()
-        unshuffle = B.conv(mode='F')
-        head = B.conv(48, nc, mode='C')
-        body = [B.ResBlock(nc, nc, mode='RCRC') for _ in range(nb)]
-        tail = B.conv(nc, 1, mode='C')
-        self.model = B.sequential(unshuffle, head, B.sequential(*body), tail)
-
-    def forward(self, x):
-        """
-        Only the lr image. No ref images
-        """
-        # pass it through the model
-        return self.model(x)
